8becb670de65f2183c3a3ab25e307459b255f06c
Rupert Nash <rupert.nash@ed.ac.uk>
2014-06-11 11:50:15 +0100

    Update reconstruct system to deal with the inter-process off-diagonal elements now output.

--- reconstructSystem/reconstructSystem.C
+++ reconstructSystem/reconstructSystem.C
@@ -135,6 +135,9 @@ int main(int argc, char* argv[])
 	  PtrList<labelList> upperAddrs(nProcs);
 	  PtrList<labelList> lowerAddrs(nProcs);
 	  
+	  List<PtrList<labelList> > localCellIds(nProcs);
+	  List<PtrList<scalarField> > coeffs(nProcs);
+	  
 	  label totalPoints = 0,
 	    totalOffDiag = 0;
 	  bool anyUpper = false,
@@ -201,6 +204,49 @@ int main(int argc, char* argv[])
 		      << exit(FatalError);
 		}
 	      
+	      // Get the interprocess boundary data
+	      dictionary& interfaceDict = sysDict.subDict("interfaces");
+	      // Resize the ptrLists
+	      localCellIds[procI].resize(nProcs);
+	      coeffs[procI].resize(nProcs);
+	      
+	      // Loop over the possible processors (as I can't figure out how to iterate over a dictionary)
+	      for (label procJ = 0; procJ < nProcs; ++procJ)
+		{
+		  if (procJ == procI)
+		    continue;
+		  
+		  const word key = word("processor") + name(procJ);
+		  
+		  if (interfaceDict.found(key))
+		    {
+		      dictionary& procDict = interfaceDict.subDict(key);
+		      localCellIds[procI].set(procJ, new labelList());
+		      if (!procDict.readIfPresent("localCellIds", localCellIds[procI][procJ]))
+			FatalErrorIn(args.executable())
+			  << "Missing localCellIds in system for processor " << procI
+			  << " with its boundary with processor " << procJ
+			  << exit(FatalError);
+
+		      coeffs[procI].set(procJ, new scalarField());
+		      if (!procDict.readIfPresent("coeffs", coeffs[procI][procJ]))
+			FatalErrorIn(args.executable())
+			  << "Missing coeffs in system for processor " << procI
+			  << " with its boundary with processor " << procJ
+			  << exit(FatalError);
+
+		      if (coeffs[procI][procJ].size() != localCellIds[procI][procJ].size())
+			FatalErrorIn(args.executable())
+			  << "Size mismatch in inteface data in system for processor " << procI
+			  << " with its boundary with processor " << procJ
+			  << exit(FatalError);
+		      
+		      // Protect against double-counting of off diagonal elements.
+		      if (procI > procJ)
+			nOffDiag += coeffs[procI][procJ].size();
+		    }
+		}
+	      
 	      totalPoints += sources[procI].size();
 	      totalOffDiag += nOffDiag;
 	    }
@@ -218,42 +264,119 @@ int main(int argc, char* argv[])
 
 	  for (label procI = 0; procI < nProcs; ++procI)
 	    {
-	      const List<label>* addresses = procMeshes.cellProcAddressing()(procI);
-	      if (addresses->size() != sources(procI)->size())
+	      const labelIOList& addressesProcI = procMeshes.cellProcAddressing()[procI];
+	      
+	      // To deal with deleting the loaded data aggressively, we're going to
+	      // NULL out the entry grab an autoPtr to the item. The item will then
+	      // be freed once the autoPtr goes out of scope at the end of the loop
+	      // iteration.
+	      autoPtr<scalarField> src = sources.set(procI, NULL);
+	      autoPtr<scalarField> dia = diags.set(procI, NULL);
+	      
+	      if (addressesProcI.size() != src->size())
 		FatalErrorIn(args.executable())
 		  << "Size mismatch"
 		  << exit(FatalError);
-	      for (label i = 0; i < sources(procI)->size(); ++i)
+	      
+	      // Copy over the source and diagonal, translating the
+	      // addressesProcI from proc-local to global.
+	      for (label i = 0; i < src->size(); ++i)
 		{
-		  totalSource[(*addresses)[i]] = sources[procI][i];
-		  totalDiag[(*addresses)[i]] = diags[procI][i];
+		  totalSource[addressesProcI[i]] = src()[i];
+		  totalDiag[addressesProcI[i]] = dia()[i];
 		}
 	      
-	      label nOff = 0;
-	      bool hasUpper = (uppers(procI) != NULL);
+	      // Now deal with the processor internal off-diagonal elements.
+	      // Upper, if it's there
+	      autoPtr<scalarField> upp = uppers.set(procI, NULL);
+	      bool hasUpper = upp.valid();
 	      if (hasUpper)
-		nOff = uppers[procI].size();
-	      bool hasLower = (lowers(procI) != NULL);
-	      if (hasLower)
-		nOff = lowers[procI].size();
-	      
-	      bool hasOffDiag = hasUpper || hasLower;
-	      for (label i = 0; i < nOff; ++i)
 		{
-		  if (hasUpper)
-		    totalUpper[offDiagI] = uppers[procI][i];
-		  if (hasLower)
-		    totalLower[offDiagI] = lowers[procI][i];
-		  
-		  totalLowerAddr[offDiagI] = (*addresses)[
-							  lowerAddrs[procI][i]
-							  ];
-		  totalUpperAddr[offDiagI] = (*addresses)[
-							  upperAddrs[procI][i]
-							  ];
-		  ++offDiagI;
+		  memcpy(totalUpper.data() + offDiagI,
+			 upp->cdata(),
+			 upp->byteSize());
 		}
-	    }
+	      
+	      // Lower, if it's there
+	      autoPtr<scalarField> low = lowers.set(procI, NULL);
+	      bool hasLower = low.valid();
+	      if (hasLower)
+		{
+		  memcpy(totalLower.data() + offDiagI,
+			 low->data(),
+			 low->byteSize());
+		}
+	      
+	      // Addressing if either upper or lower is present
+	      autoPtr<labelList> ua = upperAddrs.set(procI, NULL);
+	      autoPtr<labelList> la = lowerAddrs.set(procI, NULL);
+	      if (hasUpper || hasLower)
+		{
+		  // Grab references to give the compiler a chance.
+		  const labelList& uar = ua();
+		  const labelList& lar = la();
+		  // Copy the addressing over, translating from local
+		  // to global. Also note that we increment both i and
+		  // offDiagI.
+		  for (label i = 0; i < ua->size(); ++i, ++offDiagI)
+		    {
+		      totalUpperAddr[offDiagI] = addressesProcI[uar[i]];
+		      totalLowerAddr[offDiagI] = addressesProcI[lar[i]];
+		    }
+		}
+	      
+	      // Now onto the inter-process off-diagonal elements. We
+	      // loop over all procJ > procI to ensure we don't double
+	      // count. (We have to deal with each pairing exactly
+	      // once.)
+	      for (label procJ = procI + 1; procJ < nProcs; ++ procJ)
+		{
+		  autoPtr<labelList> cellIdsIJ = localCellIds[procI].set(procJ, NULL);
+		  autoPtr<labelList> cellIdsJI = localCellIds[procJ].set(procI, NULL);
+		  
+		  autoPtr<scalarField> coeffsIJ = coeffs[procI].set(procJ, NULL);
+		  autoPtr<scalarField> coeffsJI = coeffs[procJ].set(procI, NULL);
+		  
+		  if (cellIdsIJ.valid())
+		    {
+		      // Transpose must exist
+		      if (!cellIdsJI.valid())
+			FatalErrorIn(args.executable())
+			  << "Missing expected boundary data (due to symmetry) from " 
+			  << procJ << " to " << procI
+			  << exit(FatalError);
+		      
+		      // Sizes of corresponding patches must match
+		      if (cellIdsIJ->size() != cellIdsJI->size())
+			FatalErrorIn(args.executable())
+			  << "Patch size mismatch between processors " 
+			  << procI << " and " << procJ
+			  << exit(FatalError);
+		      
+		      const labelIOList& addressesProcJ = procMeshes.cellProcAddressing()[procJ];
+		      // Get refs to help the compiler...
+		      const scalarField& cIJ = coeffsIJ();
+		      const scalarField& cJI = coeffsJI();
+		      const labelList& idIJ = cellIdsIJ();
+		      const labelList& idJI = cellIdsJI();
+		      
+		      
+		      for (label i = 0; i < cellIdsIJ->size(); ++i, ++offDiagI)
+			{
+			  // Recall that we have to negate the coefficient as we copy them in.
+			  // The code in processorFvPatchField.C makes clear which way round these should go.
+			  if (anyUpper)
+			    totalUpper[offDiagI] = -cIJ[i];
+			  if (anyLower)
+			    totalLower[offDiagI] = -cJI[i];
+			  
+			  totalUpperAddr[offDiagI] = addressesProcI[idIJ[i]];
+			  totalLowerAddr[offDiagI] = addressesProcJ[idJI[i]];
+			}
+		    }
+		}
+	      
+	    } // for (procI... )
 
 	  // Prepare a dict for writing the reconstructed one back.
 	  IOdictionary systemDict(
